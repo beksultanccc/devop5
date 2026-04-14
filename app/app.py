7af1932aa3b23e8534f696f1ae16953bbb1b0d04
Beksultan <beko040406@gmail.com>
2026-04-14 23:43:36 +0500

    Update app.py

--- app/app.py
+++ app/app.py
@@ -9,5 +9,17 @@ def home():
         "hits": 1
     })
 
+@app.route("/add-user")
+def add_user():
+    return jsonify({
+        "message": "Пайдаланушы қосылды"
+    })
+
+@app.route("/users")
+def users():
+    return jsonify({
+        "users": ["JenkinsUser1", "JenkinsUser2", "JenkinsUser3"]
+    })
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
